ff62d66d1be034e4c2bcc7bed6019eb10fd7f01c
martrod <mrodriguez@makingsense.com>
2021-09-03 13:53:59 -0300

    First console app from
    MS Academy Rent a car console app

---
+++ RentACar/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RentACar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Create a new car");
+            var carCRUD = new CarCRUD();
+            carCRUD.CreateCar();
+            Console.WriteLine("Input a car Id to display a car");
+            carCRUD.ReadCar(Console.ReadLine());
+
+        }
+    }
+}
