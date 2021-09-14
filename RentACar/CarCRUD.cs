9bcfaff07a68b11c4ba6ea374ea0f0995a3f19d1
martrod <mrodriguez@makingsense.com>
2021-09-14 11:07:08 -0300

    Add a Menu
    several changes were made
    -New class Menu with 2 methods
    	-MenuUi
    	-SwitchForMenu
    -New App.config

--- RentACar/CarCRUD.cs
+++ RentACar/CarCRUD.cs
@@ -2,92 +2,90 @@
 using RentACar;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.IO;
+using System.Linq;
 
+public class CarCRUD
+{
+    List<Car> carList = new List<Car>();
+    public static string fileName = ConfigurationManager.AppSettings[("jsonFileCars")];
+    Car car = new Car();
+    public static string path = ConfigurationManager.AppSettings[("path")];
+    string jsonString = File.ReadAllText(fileName);
 
-
-    public class CarCRUD
+    public void CreateCar()
     {
-        List<Car> carList = new List<Car>();
-        public static string fileName = @"..\..\..\Data\Cars.json";
-        Car car = new Car();
-        //TODO set relative path
-        public static string path = @"C:\Users\mrodriguez\Documents\MS-AcademyDotNet\MS-AcademyDotNet\RentACar\Data\Cars.json";
-        string jsonString = File.ReadAllText(fileName);
-
-        public void CreateCar()
+        carList = JsonConvert.DeserializeObject<List<Car>>(jsonString);
+        car.IdCar = Convert.ToByte(carList.Count + 1);
+        Console.WriteLine($"creating car with Id {car.IdCar}");
+        car.Availability = true;
+        Console.WriteLine("Enter ID Plate");
+        car.IdPlate = Convert.ToByte(Console.ReadLine());
+        Console.WriteLine("Enter car brand:");
+        car.Brand = Console.ReadLine();
+        Console.WriteLine("Enter car model:");
+        car.Model = Console.ReadLine();
+        Console.WriteLine("Enter the colour:");
+        car.Colour = Console.ReadLine();
+        Console.WriteLine("Enter doors amount:");
+        car.Doors = Console.ReadLine();
+        Console.WriteLine("Enter transmission type:");
+        car.Transmission = Console.ReadLine();
+        carList.Add(car);
+        Console.WriteLine("Car added successfully!");
+        string json = JsonConvert.SerializeObject(carList);
+        System.IO.File.WriteAllText(path, json);
+    }
+    public void ReadCar(String idCar)
+    {
+        carList = JsonConvert.DeserializeObject<List<Car>>(jsonString);
+        //TODO: implementar un foreach
+        for (var i = 0; i < carList.Count; i++)
         {
-            //TODO add automatic IdCar
-            carList = JsonConvert.DeserializeObject<List<Car>>(jsonString);
-            car.Availability = true;
-            Console.WriteLine("Enter ID Plate");
-            car.IdPlate = Convert.ToByte(Console.ReadLine());
-            Console.WriteLine("Enter car brand:");
-            car.Brand = Console.ReadLine();
-            Console.WriteLine("Enter car model:");
-            car.Model = Console.ReadLine();
-            Console.WriteLine("Enter the colour:");
-            car.Colour = Console.ReadLine();
-            Console.WriteLine("Enter doors amount:");
-            car.Doors = Console.ReadLine();
-            Console.WriteLine("Enter transmission type:");
-            car.Transmission = Console.ReadLine();
-            carList.Add(car);
-            Console.WriteLine("Car added successfully!");
-            string json = JsonConvert.SerializeObject(carList);
-            System.IO.File.WriteAllText(path, json);
-        }
-        public void ReadCar(string Id)
-        {
-            carList = JsonConvert.DeserializeObject<List<Car>>(jsonString);
-            for (var i = 0; i < carList.Count; i++)
+            if (Convert.ToByte(idCar) == carList[i].IdCar)
             {
-                if (Convert.ToByte(Id) == carList[i].IdPlate)
-                {
-                    Console.WriteLine($"Id Car: {carList[i].IdCar}");
-                    Console.WriteLine($"Availability: {carList[i].Availability}");
-                    Console.WriteLine($"Id plate: {carList[i].IdPlate}");
-                    Console.WriteLine($"Brand: {carList[i].Brand}");
-                    Console.WriteLine($"Model: {carList[i].Model}");
-                    Console.WriteLine($"Door number: {carList[i].Doors}");
-                    Console.WriteLine($"Colour: {carList[i].Colour}");
-                    Console.WriteLine($"Transmission type: {carList[i].Transmission}");
-                }
-            }
-        }
-        public void Update(string Id)
-        {
-            carList = JsonConvert.DeserializeObject<List<Car>>(jsonString);
-            for (var i = 0; i < carList.Count; i++)
-            {
-                if (Convert.ToByte(Id) == (carList[i].IdPlate))
-                {
-                    Console.WriteLine("You will update a car based on its Id");
-                    Console.WriteLine("Enter availability. Answer true or false");
-                    carList[i].Availability = Convert.ToBoolean(Console.ReadLine());
-                    Console.WriteLine("Enter colour:");
-                    carList[i].Colour = Console.ReadLine();
-                    Console.WriteLine("Car Updated successfully!");
-                    string json = JsonConvert.SerializeObject(carList);
-                    System.IO.File.WriteAllText(path, json);
-                }
-            }
-        }
-
-        public void Delete(string IdPlate)
-        {
-            carList = JsonConvert.DeserializeObject<List<Car>>(jsonString);
-
-            foreach (Car element in carList)
-            {
-                if (IdPlate.Equals(element.IdPlate))
-                {
-                    Console.WriteLine($"Id plate: {IdPlate} deleted!");
-                    carList.Remove(element);
-                    string json = JsonConvert.SerializeObject(carList);
-                    System.IO.File.WriteAllText(path, json);
-                    break;
-                }
+                Console.WriteLine($"Id Car: {carList[i].IdCar}");
+                Console.WriteLine($"Availability: {carList[i].Availability}");
+                Console.WriteLine($"Id plate: {carList[i].IdPlate}");
+                Console.WriteLine($"Brand: {carList[i].Brand}");
+                Console.WriteLine($"Model: {carList[i].Model}");
+                Console.WriteLine($"Door number: {carList[i].Doors}");
+                Console.WriteLine($"Colour: {carList[i].Colour}");
+                Console.WriteLine($"Transmission type: {carList[i].Transmission}");
             }
         }
     }
+    public void UpdateCar(string idCar)
+    {
+        carList = JsonConvert.DeserializeObject<List<Car>>(jsonString);
+        //TODO: implementar un foreach
+        for (var i = 0; i < carList.Count; i++)
+        {
+            if (Convert.ToByte(idCar) == (carList[i].IdCar))
+            {
+                Console.WriteLine("You will update a car based on its Id");
+                Console.WriteLine("Enter availability. Answer true or false");
+                carList[i].Availability = Convert.ToBoolean(Console.ReadLine());
+                Console.WriteLine("Enter colour:");
+                carList[i].Colour = Console.ReadLine();
+                Console.WriteLine("Car Updated successfully!");
+                string json = JsonConvert.SerializeObject(carList);
+                System.IO.File.WriteAllText(path, json);
+            }
+        }
+    }
+
+    public void Delete(int idCar)
+    {
+        carList = JsonConvert.DeserializeObject<List<Car>>(jsonString);
+        foreach (var element in carList.Where(element => idCar.Equals(element.IdCar)))
+        {
+            Console.WriteLine($"Id car: {idCar} deleted!");
+            carList.Remove(element);
+            string json = JsonConvert.SerializeObject(carList);
+            System.IO.File.WriteAllText(path, json);
+            break;
+        }
+    }
+}
--- RentACar/Program.cs
+++ RentACar/Program.cs
@@ -6,13 +6,21 @@ namespace RentACar
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Create a new car");
-            var carCRUD = new CarCRUD();
-            carCRUD.CreateCar();
-            Console.WriteLine("Input a car Id to display a car");
-            carCRUD.ReadCar(Console.ReadLine());
-            Console.WriteLine("Edit a car by Id");
-            carCRUD.Update(Console.ReadLine());
+            //TODO: Hacer un case
+
+            Menu menu = new Menu();
+            //menu.MenuUi();
+            menu.SwitchForMenu();
+            //Console.WriteLine("Create a new car");
+            //var carCRUD = new CarCRUD();
+            //carCRUD.CreateCar();
+            //Console.WriteLine("Input a car Id to display a car");
+            //carCRUD.ReadCar(Console.ReadLine());
+            //Console.WriteLine("Edit a car by Id");
+            //carCRUD.UpdateCar(Console.ReadLine());
+            //Console.WriteLine("Delete a car by Id");
+            ////TODO try to not convert. Instead set as a var in Car class
+            //carCRUD.Delete(Convert.ToInt32(Console.ReadLine()));
         }
     }
 }
