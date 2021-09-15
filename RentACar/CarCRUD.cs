d6cbad7898ba0b4a96546cabc75b9b2d63a82289
martrod <mrodriguez@makingsense.com>
2021-09-15 10:34:27 -0300

    Add logic erase in CarCRUD

--- RentACar/CarCRUD.cs
+++ RentACar/CarCRUD.cs
@@ -79,8 +79,9 @@ public class CarCRUD
         carList = JsonConvert.DeserializeObject<List<Car>>(jsonString);
         foreach (var element in carList.Where(element => idCar.Equals(element.IdCar)))
         {
+            element.IsCarDeleted = true;
             Console.WriteLine($"Id car: {idCar} deleted!");
-            carList.Remove(element);
+            //carList.Remove(element);
             string json = JsonConvert.SerializeObject(carList);
             System.IO.File.WriteAllText(path, json);
             break;
